d39d795c949a51ee096181611f5b48f4cf755e22
Adrian Pabe <adrianpabs14@gmail.com>
2021-01-30 06:54:41 +0800

    Log validation

--- models/User.js
+++ models/User.js
@@ -1,17 +1,19 @@
 const {Schema, model} = require('mongoose');
+const { isEmail } = require('validator');
 
 const userSchema = new Schema({
     email: {
         type: String,
         unique: true,
-        required: true,
         lowercase: true,
+        required: [true, 'Please enter an email'],
+        validate: [isEmail, 'Please enter a valid email'],
     },
 
     password: {
         type: String,
-        required: true,
-        minLength: true,
+        required: [true, 'Please enter a password'],
+        minlength: [6, 'Minimum password length is 6 characters'],
     },
 })
 
