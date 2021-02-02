57e72f847ee8a6bb99a067fd216c320d6bf33465
Adrian Pabe <adrianpabs14@gmail.com>
2021-02-02 18:12:59 +0800

    Log login process

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,13 +7,23 @@ const handleErrors = (err) => {
     console.log(err.message, err.code);
     let errors = { email: '', password: '' };
 
+    // incorrect email
+    if  (err.message === 'incorrect email') {
+        errors.email = 'email is not registered';
+    }
+
+    // incorrect password
+    if  (err.message === 'incorrect password') {
+        errors.password = 'password is incorrect';
+    }
+
     // duplicate error code
-    if (err.code === 11000){
+    if (err.code === 11000) {
         errors.email = 'Email is already registered';
     }
 
     // validation errors
-    if (err.message.includes('User validation failed')){
+    if (err.message.includes('User validation failed')) {
         Object.values(err.errors).forEach(({ properties }) => {
             errors[properties.path] = properties.message;
         })
@@ -42,10 +52,12 @@ module.exports.signup_post = async (req, res) => {
     try {
         const user = await User.create({email, password});
         const token = createToken(user._id);
+
         res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 })
         res.status(201).json({ user: user._id });
     } catch (err) {
         const errors = handleErrors(err);
+
         res.status(400).json({
             errors
         });
@@ -55,5 +67,17 @@ module.exports.signup_post = async (req, res) => {
 module.exports.login_post = async (req, res) => {
     const  { email, password } = req.body;
 
-    res.send('user login');
+    try {
+        const user = await User.login(email, password);
+        const token = createToken(user._id);
+
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 })
+        res.status(200).json({ user: user._id });
+    }
+    catch (err) {
+        const errors = handleErrors(err);
+
+        console.log(err);
+        res.status(400).json({ errors });
+    }
 }
